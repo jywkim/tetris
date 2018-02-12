7c6b61184f0a80de2a01c2d25658ca4185d9deab
jywkim <jkim589@gmail.com>
2018-02-12 01:35:00 -0500

    add playerRotate function and bind to Q and W

--- tetris.js
+++ tetris.js
@@ -79,6 +79,21 @@ function playerMove(dir) {
   }
 }
 
+function playerRotate(dir) {
+  const pos = player.pos.x
+  let offset = 1;
+  rotate(player.matrix, dir);
+  while (collide(arena, player)) {
+    player.pos.x += offset;
+    offset = -(offset + (offset > 0 ? 1 : -1));
+    if (offset > player.matrix[0].length) {
+      rotate(player.matrix, -dir);
+      player.pos.x = pos;
+      return;
+    }
+  }
+}
+
 function rotate(matrix, dir) {
   for (let y = 0; y < matrix.length; ++y) {
     for (let x = 0; x < y; ++x) {
@@ -129,6 +144,10 @@ document.addEventListener('keydown', event => {
     playerMove(1);
   } else if (event.keyCode === 40) {
     playerDrop();
+  } else if (event.keyCode === 81) {
+    playerRotate(-1);
+  } else if (event.keyCode === 87) {
+    playerRotate(1);
   }
 });
 
